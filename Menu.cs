90e3f44d272167ba42d19077e377bfc0aeeb8df8
Danielly Araujo <48495720+daniellyaraujo@users.noreply.github.com>
2019-07-12 13:28:07 -0300

    Add files via upload

---
+++ Menu.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace cadastrodeclientesebd
+{
+    public partial class Menu : Form
+    {
+        public Menu() 
+        {
+            InitializeComponent();
+        }
+
+        private void btcadclientes_Click(object sender, EventArgs e)
+        {
+            cadastrocliente clientes = new cadastrocliente();
+            clientes.ShowDialog();
+        }
+    }
+}
---
+++ cadastrocliente.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace cadastrodeclientesebd
+{
+    public partial class cadastrocliente : Form
+    {
+        public cadastrocliente()
+        {
+            InitializeComponent();
+        }
+
+        private void tbclientesBindingNavigatorSaveItem_Click(object sender, EventArgs e)
+        {
+            this.Validate();
+            this.tbclientesBindingSource.EndEdit();
+            this.tableAdapterManager.UpdateAll(this.bdSistemaGeralDataSet);
+
+        }
+
+        private void cadastrocliente_Load(object sender, EventArgs e)
+        {
+            // TODO: esta linha de código carrega dados na tabela 'bdSistemaGeralDataSet.tbclientes'. Você pode movê-la ou removê-la conforme necessário.
+            this.tbclientesTableAdapter.Fill(this.bdSistemaGeralDataSet.tbclientes);
+
+        }
+
+        private void tbclientesDataGridView_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+    }
+}
